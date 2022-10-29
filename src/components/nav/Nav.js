15058c407f0ff8ac1c2560cb9946dafabbdb94b9
Nancy <nancy62924@gmail.com>
2022-10-29 18:09:45 +0800

    feat: add about component

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,44 @@
-import React from "react";
+import { useState } from "react";
 import "./nav.css";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineBook,
+  AiOutlineMessage,
+} from "react-icons/ai";
+import { RiServiceLine } from "react-icons/ri";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+
+  const navItems = [
+    { link: "#", icon: <AiOutlineHome />, id: "#" },
+    { link: "#about", icon: <AiOutlineUser />, id: "#about" },
+    { link: "#experience", icon: <AiOutlineBook />, id: "#experience" },
+    { link: "#services", icon: <RiServiceLine />, id: "#services" },
+    { link: "#contact", icon: <AiOutlineMessage />, id: "#contact" },
+  ];
+
+  const handleClick = (id) => {
+    setActiveNav(id);
+  };
+
+  return (
+    <nav>
+      {navItems.map(({ link, icon, id }) => {
+        return (
+          <a
+            href={link}
+            key={id}
+            className={activeNav === id ? "active" : null}
+            onClick={() => handleClick(id)}
+          >
+            {icon}
+          </a>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
